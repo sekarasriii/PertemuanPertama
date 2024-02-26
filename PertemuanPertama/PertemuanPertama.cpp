af5580021bc236e857fe1303fb0033aea9de0928
sekarasriii <sekareeesh@gmail.com>
2024-02-26 11:33:05 +0700

    display 'Masukan jejari

--- PertemuanPertama/PertemuanPertama.cpp
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -5,16 +5,15 @@ int main()
 {
    // begin
     // numeric nJejari, nLuas
-    // display ' masukan jejari= '
+    // display ' Masukan jejari= '
     // accept nJejari
     // compute nLuas = 3.14 * r * r
     // display ' Luasnya= ' + nLuas
     //end
 
     double nJejari, nLuas;
-    
+    cout << "Masukan Jejari = ";
 
-    
     system("pause");
 
 }
